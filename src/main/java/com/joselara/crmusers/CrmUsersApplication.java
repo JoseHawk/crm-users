0fa59fab04d415310f4c2664ce9c829b992d6d81
Jose Lara <jose.lara@uxsmobile.com>
2019-04-09 23:20:49 +0200

    Initial commit

---
+++ src/main/java/com/joselara/crmusers/CrmUsersApplication.java
@@ -0,0 +1,13 @@
+package com.joselara.crmusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmUsersApplication.class, args);
+    }
+
+}
